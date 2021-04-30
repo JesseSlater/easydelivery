a340c9e90311dbe433a652fae39120fdb401170c
JesseSlater <40251418+JesseSlater@users.noreply.github.com>
2021-04-29 20:26:58 -0500

    Add files via upload
    Insert Orders rearranged; now under their respective tables.

--- EasyDeliveryIntAUto.sql
+++ EasyDeliveryIntAUto.sql
@@ -9,6 +9,56 @@ CREATE TABLE PERSON (
     Address VARCHAR(30),
     PRIMARY KEY (P_ID)
 );
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jackie', NULL,'Billy','1970-05-020','4714 JasonJill Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jester', NULL,'James','1995-04-05','503 TawnyTimmy Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Billy','1950-08-018','4841 JarnoldJackie Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tawny', NULL,'Tron','1974-03-017','976 TronJester Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jason', NULL,'Simon','1963-07-020','4456 SaulJack Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Brawny', NULL,'Tammy','2004-08-020','4669 BrawnySaul Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Baluga', NULL,'Jester','2000-06-02','4206 JasonSink Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Billy', NULL,'Sangria','2006-04-04','3460 JackBranson Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Jack','1970-02-025','2428 SanjiJason Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Sangria','2008-03-010','297 JackJester Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Sink', NULL,'Jaquan','1986-08-012','794 JillBaluga Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jill', NULL,'Shaun','1972-02-06','4513 TammyJill Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jill', NULL,'Smoker','1960-04-07','2641 JesterBilly Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Branson', NULL,'Simon','1976-04-012','4287 BalugaBrawny Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Billy', NULL,'Sink','1976-05-012','2258 BrawnySanji Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Shrek', NULL,'Jiminy','1970-08-014','883 SanjiShepton Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jamie', NULL,'Timmy','1984-04-019','3941 BalugaJack Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jamie', NULL,'Jaquan','1998-07-06','981 BillyTrunk Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Johnny', NULL,'James','1971-07-013','4290 JarnoldJill Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Shrek', NULL,'Jack','1990-04-015','3462 TimmyShepton Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jackie', NULL,'Jackie','1951-05-01','2434 BransonBilly Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Jaquan','1993-01-07','3749 SinkJohnny Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Timmy', NULL,'Timmy','1962-04-018','4702 JamieJohnny Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Saul', NULL,'Jester','1996-05-015','2029 JamieJaquan Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jamie', NULL,'Tom','1996-01-03','1861 ShaunJackie Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jackie', NULL,'Jackie','2002-07-021','3629 SanjiJames Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tawny', NULL,'Baluga','1974-02-03','2833 BillyJarnold Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tron', NULL,'Trunk','1975-02-06','3637 SaulShrek Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tron', NULL,'Jaquan','1955-05-012','3314 JamesTom Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Branson', NULL,'Smoker','1976-03-020','4927 TimmyTom Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Trunk','1986-05-012','857 BrawnySanji Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Sink', NULL,'Jamie','1999-06-04','2250 JasonSaul Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Smoker', NULL,'Branson','1989-06-021','2357 JesterJiminy Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tawny', NULL,'Sanji','1988-01-020','2871 TawnySanji Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Saul', NULL,'Saul','1999-01-021','4464 TronJohnny Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Billy', NULL,'Timmy','2005-08-05','4039 JarnoldSimon Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Branson', NULL,'Tron','1993-02-09','4961 SaulJiminy Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jill', NULL,'Jester','1968-02-023','2419 JackieTom Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Timmy', NULL,'Shaun','1967-02-02','4635 TimmyJaquan Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jason', NULL,'James','2004-04-09','4090 SinkSaul Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Trunk', NULL,'Jill','1972-06-03','1508 SmokerBrawny Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jaquan', NULL,'James','1959-06-019','325 BobbyShepton Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Trunk', NULL,'Jaquan','1993-02-011','2309 TomJill Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tom', NULL,'Jiminy','1976-05-08','773 JackJiminy Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tron', NULL,'Jiminy','1957-01-019','3528 JillSangria Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jason', NULL,'Sangria','1990-01-09','2026 SaulSangria Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tawny', NULL,'Jason','2002-03-023','3732 JamieJames Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tron', NULL,'Tammy','2006-04-05','3829 SheptonTawny Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Saul', NULL,'Tom','1964-06-07','2161 TammyShepton Street');
+INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jack', NULL,'Jester','2000-02-05','2446 JackBrawny Street');
 
 CREATE TABLE PHONE_NUMBER (
     Phone_number CHAR(10) NOT NULL,
@@ -48,6 +98,7 @@ CREATE TABLE GENDER (
     Gender VARCHAR(30),
     PRIMARY KEY (Gender_ID)
 );
+INSERT INTO `gender` (`Gender_ID`, `Gender`) VALUES (NULL, 'Male'), (NULL, 'Female') ;
 
 CREATE TABLE START_DATE (
     Start_Date_ID INT NOT NULL auto_increment,
@@ -197,6 +248,14 @@ CREATE TABLE MAKE (
     Make VARCHAR(30),
     PRIMARY KEY (Make_ID)
 );
+INSERT INTO MAKE
+    (Make_ID, Make)
+VALUES
+    (1, 'Ford'),
+    (2, 'Chevrolet'),
+    (3, 'Toyota'),
+    (4, 'Honda'),
+    (5, 'GMC');
     
 CREATE TABLE MODEL (
     Model_ID INT NOT NULL AUTO_INCREMENT,
@@ -206,6 +265,15 @@ CREATE TABLE MODEL (
     FOREIGN KEY (Make_ID)
         REFERENCES MAKE (Make_ID)
 );
+
+INSERT INTO MODEL
+    (Model_ID, Model, Make_ID)
+VALUES
+    (1, "F-150", (SELECT Make_ID FROM MAKE WHERE Make='Ford')),
+    (2, "Silverado", (SELECT Make_ID FROM MAKE WHERE Make='Chevrolet')),
+    (3, "Rav4", (SELECT Make_ID FROM MAKE WHERE Make='Toyota')),
+    (4, "CR-V", (SELECT Make_ID FROM MAKE WHERE Make='Honda')),
+    (5, "Camry", (SELECT Make_ID FROM MAKE WHERE Make='Toyota'));
     
 CREATE TABLE VEHICLE (
     Plate_number CHAR(7) NOT NULL,
@@ -426,59 +494,6 @@ CREATE TABLE SHOP_SCHEDULE (
 );
 
 
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jackie', NULL,'Billy','1970-05-020','4714 JasonJill Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jester', NULL,'James','1995-04-05','503 TawnyTimmy Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Billy','1950-08-018','4841 JarnoldJackie Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tawny', NULL,'Tron','1974-03-017','976 TronJester Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jason', NULL,'Simon','1963-07-020','4456 SaulJack Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Brawny', NULL,'Tammy','2004-08-020','4669 BrawnySaul Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Baluga', NULL,'Jester','2000-06-02','4206 JasonSink Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Billy', NULL,'Sangria','2006-04-04','3460 JackBranson Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Jack','1970-02-025','2428 SanjiJason Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Sangria','2008-03-010','297 JackJester Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Sink', NULL,'Jaquan','1986-08-012','794 JillBaluga Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jill', NULL,'Shaun','1972-02-06','4513 TammyJill Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jill', NULL,'Smoker','1960-04-07','2641 JesterBilly Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Branson', NULL,'Simon','1976-04-012','4287 BalugaBrawny Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Billy', NULL,'Sink','1976-05-012','2258 BrawnySanji Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Shrek', NULL,'Jiminy','1970-08-014','883 SanjiShepton Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jamie', NULL,'Timmy','1984-04-019','3941 BalugaJack Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jamie', NULL,'Jaquan','1998-07-06','981 BillyTrunk Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Johnny', NULL,'James','1971-07-013','4290 JarnoldJill Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Shrek', NULL,'Jack','1990-04-015','3462 TimmyShepton Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jackie', NULL,'Jackie','1951-05-01','2434 BransonBilly Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Jaquan','1993-01-07','3749 SinkJohnny Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Timmy', NULL,'Timmy','1962-04-018','4702 JamieJohnny Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Saul', NULL,'Jester','1996-05-015','2029 JamieJaquan Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jamie', NULL,'Tom','1996-01-03','1861 ShaunJackie Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jackie', NULL,'Jackie','2002-07-021','3629 SanjiJames Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tawny', NULL,'Baluga','1974-02-03','2833 BillyJarnold Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tron', NULL,'Trunk','1975-02-06','3637 SaulShrek Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tron', NULL,'Jaquan','1955-05-012','3314 JamesTom Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Branson', NULL,'Smoker','1976-03-020','4927 TimmyTom Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tammy', NULL,'Trunk','1986-05-012','857 BrawnySanji Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Sink', NULL,'Jamie','1999-06-04','2250 JasonSaul Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Smoker', NULL,'Branson','1989-06-021','2357 JesterJiminy Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tawny', NULL,'Sanji','1988-01-020','2871 TawnySanji Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Saul', NULL,'Saul','1999-01-021','4464 TronJohnny Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Billy', NULL,'Timmy','2005-08-05','4039 JarnoldSimon Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Branson', NULL,'Tron','1993-02-09','4961 SaulJiminy Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jill', NULL,'Jester','1968-02-023','2419 JackieTom Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Timmy', NULL,'Shaun','1967-02-02','4635 TimmyJaquan Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jason', NULL,'James','2004-04-09','4090 SinkSaul Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Trunk', NULL,'Jill','1972-06-03','1508 SmokerBrawny Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jaquan', NULL,'James','1959-06-019','325 BobbyShepton Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Trunk', NULL,'Jaquan','1993-02-011','2309 TomJill Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tom', NULL,'Jiminy','1976-05-08','773 JackJiminy Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tron', NULL,'Jiminy','1957-01-019','3528 JillSangria Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jason', NULL,'Sangria','1990-01-09','2026 SaulSangria Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tawny', NULL,'Jason','2002-03-023','3732 JamieJames Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Tron', NULL,'Tammy','2006-04-05','3829 SheptonTawny Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Saul', NULL,'Tom','1964-06-07','2161 TammyShepton Street');
-INSERT INTO `person` (`P_ID`, `First_name`, `Middle_name`, `Last_name`, `Date_of_birth`, `Address`)VALUES (NULL,'Jack', NULL,'Jester','2000-02-05','2446 JackBrawny Street');
-
-
-INSERT INTO `gender` (`Gender_ID`, `Gender`) VALUES (NULL, 'Male'), (NULL, 'Female') ;
 
 INSERT INTO CUSTOMER
     (Customer_ID, P_ID)
@@ -532,20 +547,3 @@ VALUES
     (13, 14),
     (14, 15);
 
-INSERT INTO MAKE
-    (Make_ID, Make)
-VALUES
-    (1, 'Ford'),
-    (2, 'Chevrolet'),
-    (3, 'Toyota'),
-    (4, 'Honda'),
-    (5, 'GMC');
-
-INSERT INTO MODEL
-    (Model_ID, Model, Make_ID)
-VALUES
-    (1, "F-150", (SELECT Make_ID FROM MAKE WHERE Make='Ford')),
-    (2, "Silverado", (SELECT Make_ID FROM MAKE WHERE Make='Chevrolet')),
-    (3, "Rav4", (SELECT Make_ID FROM MAKE WHERE Make='Toyota')),
-    (4, "CR-V", (SELECT Make_ID FROM MAKE WHERE Make='Honda')),
-    (5, "Camry", (SELECT Make_ID FROM MAKE WHERE Make='Toyota'));

b3bb4960b30008304e55068fee701b56c800b841
JesseSlater <40251418+JesseSlater@users.noreply.github.com>
2021-04-30 16:18:34 -0500

    Add files via upload

--- QeQueries.sql
+++ QeQueries.sql
@@ -1,5 +1,17 @@
 USE EasyDelivery;
 
+
+/*D 1*/
+CREATE VIEW dOne AS
+SELECT p.First_name, p.Last_name, SUM(Total_balance) as sumTot
+FROM  Person p, CUSTOMER c, ORDERS o
+WHERE c.Customer_ID = o.Customer_ID
+AND p.p_ID = c.Customer_ID
+group by c.Customer_ID
+order by sumTot DESC
+limit 3;
+
+/*E 1*/
 SELECT DISTINCT First_name, Middle_name, Last_name
 FROM PERSON p, Employee E, AREA_MANAGER A 
 WHERE p.P_ID = E.P_ID
@@ -8,4 +20,3 @@ FROM DELIVERER
 GROUP BY Manager_ID
 ORDER BY count(*) desc
 LIMIT 1);
-

8c3878f749c0e4903ab9a0f203ecd9db135948b2
Charles Dungy <charles.dungy@utdallas.edu>
2021-05-10 16:05:15 -0500

    updated queries to run with MySQL default settings

--- QeQueries.sql
+++ QeQueries.sql
@@ -1,10 +1,8 @@
-USE EasyDelivery;
+Use EasyDelivery;
 
-
-/* 
+/*
 D 1
-AnnualTopCustomers: Thisviewreturns the First Name, Last Name, TotalOrderAmountof the customerswho paid top 3 total amount of 
-orders (in terms of total balance in the orders)in past 1 year.
+AnnualTopCustomers: This view returns the First Name, Last Name, TotalOrderAmountof the customers who paid top 3 total amount of orders (in terms of total balance in the orders)in past 1 year.
 */
 CREATE VIEW d1 AS
 SELECT p.First_name, p.Last_name, SUM(Total_balance) as sumTot
@@ -16,7 +14,7 @@ order by sumTot DESC
 limit 3;
 
 
-/* 
+/*
 D 2
 PopularRestaurantType: This view returns the Type of restaurants that have the most number of orders in past 1 year.
 */
@@ -32,29 +30,27 @@ LIMIT 1;
 
 /*
   D 3
-  Potential Silver Member: This view returns the information of the customers (not a
-  silver member yet) who have placed orders more than 10 times in the past 1 month.
+  Potential Silver Member: This view returns the information of the customers (not a silver member yet) who have placed orders more than 10 times in the past 1 month.
 */
 CREATE VIEW d3 AS
 SELECT C.Customer_ID
 FROM CUSTOMER C, ORDERS O
-WHERE EXISTS (SELECT * 
-	      FROM ORDINARY_CUSTOMER OC 
-	      WHERE OC.Customer_ID=C.Customer_ID)
+WHERE EXISTS (SELECT *
+      	FROM ORDINARY_CUSTOMER OC
+      	WHERE OC.Customer_ID=C.Customer_ID)
 AND O.Customer_ID = C.Customer_ID
 AND O.Order_Date BETWEEN SUBDATE(CURDATE(), INTERVAL 1 MONTH) AND NOW()
 GROUP BY C.Customer_ID
 HAVING COUNT(*) > 10;
 
+
 /*
   D 4
-  Best Area Manager: This view returns the information of the area manager who
-  successfully made the most number of contracts with shops in her/his working area in
-  past 1 year.
+  Best Area Manager: This view returns the information of the area manager who successfully made the most number of contracts with shops in her/his working area in past 1 year.
 */
 CREATE VIEW d4 AS
 SELECT Manager_Employee_ID
-FROM 
+FROM
 (SELECT M.Employee_ID as Manager_Employee_ID, COUNT(*) AS Num_of_Contracts
  FROM AREA_MANAGER M, MAKES_CONTRACTS MC
  WHERE M.Employee_ID=MC.Employee_ID
@@ -64,11 +60,9 @@ FROM
  ORDER BY Num_of_Contracts DESC
  LIMIT 1) as area_manager;
 
-
 /*
   D 5
-  Top Restaurants: This view returns the top restaurant that have the most orders in past 1 
-  month for each restaurant type.
+  Top Restaurants: This view returns the top restaurant that have the most orders in past 1 month for each restaurant type.
 */
 CREATE VIEW d5 AS
 SELECT Rest_Type as Restaurant_Type, Shop_ID, MAX(orders) as orders FROM
@@ -78,25 +72,24 @@ INNER JOIN RESTAURANT r ON rt.Rest_Type_ID = r.Rest_Type_ID
 INNER JOIN ORDERS o ON r.Shop_ID = o.Shop_ID
 WHERE o.Order_Date BETWEEN (SELECT DATE_ADD((SELECT CURDATE()), INTERVAL -1 MONTH)) AND NOW()
 GROUP BY Rest_Type, Shop_ID) sub
-GROUP BY Restaurant_Type;
-
+GROUP BY Restaurant_Type, Shop_ID;
 
 /*
   E 1
   Find the names of employee who supervises the most number of deliverers.
 */
 SELECT DISTINCT First_name, Middle_name, Last_name
-FROM PERSON p, Employee E, AREA_MANAGER A 
+FROM PERSON p, Employee E, AREA_MANAGER A
 WHERE p.P_ID = E.P_ID
-AND E.Employee_ID = (SELECT Manager_ID 
+AND E.Employee_ID = (SELECT Manager_ID
 FROM DELIVERER
 GROUP BY Manager_ID
 ORDER BY count(*) desc
 LIMIT 1);
 
-/*	
+/*    
  E 2
- Find the average number of orders placed by Potential Silver Member. 
+ Find the average number of orders placed by Potential Silver Member.
 */
 SELECT COUNT(O.Order_ID) / COUNT(DISTINCT(O.Customer_ID)) AS Avg_num_of_orders
 FROM ORDERS O, d3 D
@@ -105,8 +98,7 @@ GROUP BY O.Customer_ID;
 
 /*
  E 3
- Find all the customers who placed orders of the restaurants that belong to Popular
- Restaurant Type. Please also report the name of restaurants.
+ Find all the customers who placed orders of the restaurants that belong to Popular  Restaurant Type. Please also report the name of restaurants.
 */
 SELECT DISTINCT O.Customer_ID, S.Shop_name
 FROM ORDERS O, RESTAURANT R, SHOP_NAME S
@@ -114,12 +106,12 @@ WHERE O.Shop_ID=R.Shop_ID
 AND O.Shop_ID=S.Shop_name_ID
 AND EXISTS (SELECT * FROM RESTAURANT_TYPE RT WHERE RT.Rest_Type_ID=R.Rest_Type_ID AND RT.Rest_Type='Popular');
 
+
 /*
   E 4
-  List all the customers that have become a silver member within a month of joining the
-  system.
+  List all the customers that have become a silver member within a month of joining the system.
 */
-SELECT C.Customer_ID 
+SELECT C.Customer_ID
 FROM CUSTOMER C, ORDERS O
 WHERE EXISTS (SELECT * FROM SILVER_MEMBER S WHERE S.Customer_ID=C.Customer_ID)
 AND C.Customer_ID=O.Customer_ID
@@ -127,7 +119,7 @@ AND O.Order_Date BETWEEN SUBDATE(CURDATE(), INTERVAL 1 MONTH) AND NOW();
 
 /*
   E 5
-  Find the namesof deliverers who deliveredthe most orders in past 1 month.
+  Find the name of deliverers who delivered the most orders in past 1 month.
 */
 SELECT DISTINCT  P.first_name, P.last_name, count(*) as _Orders
 FROM Orders o, Vehicle v, Employee e, Person p
@@ -137,12 +129,11 @@ GROUP BY p.P_ID
 ORDER BY _Orders desc
 LIMIT 3;
 
-
 /*
    E  6
-   Find the restaurants that providemost promotion in past 1 month. 
+   Find the restaurants that provide most promotion in past 1 month.
 */
-SELECT DISTINCT sn.Shop_name, s.Shop_ID, count(*) as _Promotions 
+SELECT DISTINCT sn.Shop_name, s.Shop_ID, count(*) as _Promotions
 FROM SHOP_NAME sn, SHOP s, Promotion p
 WHERE p.Promo_Start_Date > (SELECT DATE_ADD((SELECT CURDATE()), INTERVAL -1 MONTH) )
 AND p.Shop_ID = s.Shop_ID AND s.Shop_Name_ID = sn.Shop_Name_ID
@@ -152,19 +143,19 @@ LIMIT 3;
 
 /*
    E  7
-   Find the customer who have place order sof all Fast Food restaurants. 
+   Find the customer who have place order sof all Fast Food restaurants.
 */
 Select  t2.Customer_ID as _CUSTOMER_ID FROM (SELECT DISTINCT c.Customer_ID, o.Shop_ID
 From RESTAURANT_TYPE rt, RESTAURANT r, Orders o, CUSTOMER c
-WHERE o.Shop_ID IN  (Select RESTAURANT.Shop_ID FROM RESTAURANT, RESTAURANT_TYPE where RESTAURANT_TYPE.Rest_Type = "Fast Food" AND RESTAURANT.Rest_Type_ID = RESTAURANT_TYPE.Rest_Type_ID) 
+WHERE o.Shop_ID IN  (Select RESTAURANT.Shop_ID FROM RESTAURANT, RESTAURANT_TYPE where RESTAURANT_TYPE.Rest_Type = "Fast Food" AND RESTAURANT.Rest_Type_ID = RESTAURANT_TYPE.Rest_Type_ID)
 AND o.Customer_ID = c.Customer_ID) t2
 GROUP BY t2.Customer_ID
-HAVING Count(*) = (SELECT COUNT( t3.Shop_ID )     
+HAVING Count(*) = (SELECT COUNT( t3.Shop_ID )	 
 FROM (Select Shop_ID FROM RESTAURANT, RESTAURANT_TYPE WHERE RESTAURANT_TYPE.Rest_Type = "Fast Food" AND RESTAURANT_TYPE.Rest_Type_ID = RESTAURANT.Rest_Type_ID) as t3);
 
 /*
    E  8
-   For each restaurant, list all the customers who placed the order, and the price of each order. 
+   For each restaurant, list all the customers who placed the order, and the price of each order.
 */
 Select DISTINCT sn.shop_name, p.first_name, p.last_name, o.Contents, o.Total_balance
 From RESTAURANT r, Orders o, CUSTOMER c, SHOP s, SHOP_NAME sn, Person p
@@ -186,19 +177,19 @@ GROUP BY A.Area
 ORDER BY Restaurant_Count DESC
 LIMIT 1) as area_count;
  
- 
 /*
    E  10
    Find the schedule of the restaurant that have the most orders in past 1 month.
 */
-SELECT ss.Shop_ID, Day_, Open_time, Closed_time 
-FROM SHOP_SCHEDULE ss INNER JOIN 
-(SELECT Shop_ID, max(orders)
+SELECT ss.Shop_ID, Day_, Open_time, Closed_time
+FROM SHOP_SCHEDULE ss INNER JOIN
+(SELECT Shop_ID, max(orders) as orders
 FROM d5
 GROUP BY Shop_ID
 ORDER BY orders desc
 LIMIT 1) sub
 ON ss.Shop_ID = sub.Shop_ID;
+
 
 /*
    E  11
@@ -223,7 +214,6 @@ GROUP BY s.Shop_ID
 ORDER BY products desc
 LIMIT 1;
 
-
 /*
 E  13
 For each product, list all the supermarket selling it, and the price of the product at the
@@ -233,6 +223,3 @@ SELECT p.Product_name, i.Shop_ID, pr.Price
 FROM PRODUCT p, INVENTORY_PRODUCT i, PRICE pr
 WHERE p.Product_ID = i.Product_ID
 AND i.Price_ID = pr.Price_ID;
-
-
-
